f6beb0aeb0eba82733232d2000005cca872bdefa
Maël Jamet <maeljamet@hotmail.com>
2024-03-08 00:05:49 +0100

    fourth

---
+++ tests/test_file.py
@@ -0,0 +1,5 @@
+from workfloz.file import my_func
+
+
+def my_func_test():
+    assert my_func() == 1
---
+++ tests/test_one.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+from typing import Dict, List
+
+
+def test_one() -> None:
+    assert 1 == 1
+
+
+def very_loooooone(a, b):
+    pass
+
+
+a: Dict = defaultdict(lambda: [])
+b: List
+
+
+class Test:
+    pass
