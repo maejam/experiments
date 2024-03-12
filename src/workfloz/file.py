615cefa4e380b7941ea6595392a1d40c3eae2d32
maejam <118557395+maejam@users.noreply.github.com>
2024-03-12 14:25:35 +0100

    feat: merge Feat/brch (#7)
    * fix: sthg
    
    * jhfjhfj
    
    * ci(pre-commit.ci) autofix
    
    * hghghg
    
    * ci(pre-commit.ci) autofix
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/workfloz/file.py
+++ src/workfloz/file.py
@@ -1,8 +1,18 @@
 from __future__ import annotations
 
+from abc import ABC, abstractmethod
+
+
+class Myclass(ABC):
+    @abstractmethod
+    def mymeth(self) -> None:
+        pass
+
+
 a = 1
 b = "toto"
 c = 42
+
 
 def my_func() -> int:
     return 1
